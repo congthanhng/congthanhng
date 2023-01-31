05804a35cc7436949db9e25069ba6e1881f21c3f
Cong Nguyen <congnguyent@nashtechglobal.com>
2023-01-31 15:08:35 +0700

    Implement Ci

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,5 @@ import 'dart:core';
 void main(List<String> arguments) {
   var dice1 = Random().nextInt(6) + 1;
   var dice2 = Random().nextInt(6) + 1;
-  print('${arguments[0]}: $dice1 - ${arguments[1]}: $dice2');
+  print('${arguments[0]}: $dice1 - $dice2');
 }
