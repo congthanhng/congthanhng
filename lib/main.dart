2d9e313e118dd2089c67c83ce065fd437b0ea3fb
Cong Nguyen <congnguyent@nashtechglobal.com>
2023-01-31 17:29:44 +0700

    Update readme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:math';
 
 import 'core/state_data.dart';
+import 'introduce.dart';
 
 String example = "battle|play|healx2|10";
 
@@ -85,6 +86,8 @@ void main(List<String> arguments) async {
         reset.dice2 = dice2;
         await File('lib/core/state.json')
             .writeAsString(jsonEncode(reset.toJson()));
+        await File('README.md')
+            .writeAsString(generateREADME(reset, canPowerful));
         return;
       } else {
         //decrease jojo HP
@@ -115,6 +118,9 @@ void main(List<String> arguments) async {
         reset.dice2 = dice2;
         await File('lib/core/state.json')
             .writeAsString(jsonEncode(reset.toJson()));
+
+        await File('README.md')
+            .writeAsString(generateREADME(reset, canPowerful));
         return;
       } else {
         //decrease dio HP
@@ -142,9 +148,12 @@ void main(List<String> arguments) async {
     resource.dice1 = dice1;
     resource.dice2 = dice2;
 
-    print('toJsonString: ${resource.toJsonString()}');
+    // print('toJsonString: ${resource.toJsonString()}');
     await File('lib/core/state.json')
         .writeAsString(jsonEncode(resource.toJson()));
+
+    await File('README.md')
+        .writeAsString(generateREADME(resource, canPowerful));
   } else
     throw Exception('The Issue is not correct with title format');
 }
@@ -153,4 +162,91 @@ Future<Map<String, dynamic>> readJsonFile(String filePath) async {
   var input = await File(filePath).readAsString();
   var map = jsonDecode(input);
   return map;
+}
+
+String generateREADME(StateData data, bool canPowerful) {
+  var isDioTurn = data.isDioTurn;
+  String afterAction = '''
+      <h2>👋 Hi, I'm Cong Nguyen 👋 </h2>
+Welcome to my Github profile! We're playing Battle game, you can join!
+
+It's the ${isDioTurn ? "**Dio Brando**" : "**Jotaro Kujo**"} team's turn.
+<table align="center">
+  <thead align="center">
+    <tr>
+      <td><b>Jotaro Kujo</b></td>
+      <td><b>Dio Brando</b></td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr align="center">
+      <td><code><img src="assets/jotaro_kujo.png" width=55%></code></td>
+      <td><code><img src="assets/dio_brando.png" width=55%></code></td>
+    </tr>
+    <tr>
+      <td>HP: ${generateHP(data.joJo.hp)} ${data.joJo.hp.toString()}/100 <br> MP: ${generateMP(data.joJo.mana)} ${data.joJo.mana.toString()}/25</td>
+      <td>HP: ${generateHP(data.dio.hp)} ${data.dio.hp.toString()}/100 <br> MP: ${generateMP(data.dio.mana)} ${data.dio.mana.toString()}/25</td>
+    </tr>
+  </tbody>
+</table>
+
+
+<p align="center">
+    ---<img src="assets/dice_black/db_1.png" width=10%>
+    ----
+    <img src="assets/dice_white/dw_1.png" width=10%>---
+</p>
+
+${isDioTurn ? "**Dio Brando**" : "**Jotaro Kujo**"} turn. You rolled a ${data.totalDice.toString()}!
+
+What would you like to do?
+
+| Choices *(pick one of them!)*                                                                                                                                                                          |
+|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| [Attack ${isDioTurn ? "Jotaro Kujo" : "Dio Brando"} with ${data.totalDice.toString()} points](https://github.com/congthanhng/congthanhng/issues/new?title=battle%7Cplay%7Cattack%7C${data.totalDice.toString()}&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.) |
+| [Heal yourself with ${data.totalDice.toString()} points](https://github.com/congthanhng/congthanhng/issues/new?title=battle%7Cplay%7Cheal%7C${data.totalDice.toString()}&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.)           |
+${canPowerful ? "| [Attack with x2 dame: ${data.totalDice * 2} points](https://github.com/congthanhng/congthanhng/issues/new?title=battle%7Cplay%7Cattackx2%7C${data.totalDice.toString()}&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.)           |" : ""}
+${canPowerful ? "| [Heal with x2 value: ${data.totalDice * 2} points](https://github.com/congthanhng/congthanhng/issues/new?title=battle%7Cplay%7Chealx2%7C${data.totalDice.toString()}&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.)           |" : ""}
+
+      ''';
+
+  var result = '''
+  $afterAction
+  $introduce
+  ''';
+  return result;
+}
+
+String generateHP(int current) {
+  int div = (current / 10).floor();
+  List<String> result = [];
+  for (int i = 1; i <= 10; i++) {
+    if (div == 0 || div >= i) {
+      result.add('█');
+    } else {
+      result.add('░');
+    }
+  }
+  return result.join('');
+}
+
+String generateMP(int current) {
+  int div = ((current / 25) * 10).floor();
+  List<String> result = [];
+  for (int i = 1; i <= 10; i++) {
+    if (current == 25) {
+      result.add('█');
+    } else if (div == 0 && current == 0) {
+      result.add('░');
+    } else if (div == 0 && current > 0) {
+      result.add('█');
+    } else {
+      if (div == 0 || div >= i) {
+        result.add('█');
+      } else {
+        result.add('░');
+      }
+    }
+  }
+  return result.join('');
 }
